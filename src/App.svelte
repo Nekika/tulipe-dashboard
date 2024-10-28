e1959c24efd47bde48b839a67d8c37e82f2b176c
Nekika <hugo.jahnke@gmail.com>
2024-10-28 18:12:06 +0100

    POC

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
+  type Event = { datetime: string; type: string };
+
+  let events = $state<Event[]>([]);
+
+  const promise = fetch("http://localhost:9898/events")
+    .then((response) => response.json())
+    .then((data) => (events = data))
+    .finally(() => {
+      new WebSocket("ws://localhost:9898/events/stream").addEventListener(
+        "message",
+        (message) => {
+          const event: Event = JSON.parse(message.data);
+          events = [event, ...events];
+        },
+      );
+    });
 </script>
 
 <main>
-  <h1>Hello, Tulipe!</h1>
+  <h3>Events</h3>
+  {#await promise}
+    Loading events...
+  {:then}
+    <ul>
+      {#each events as event}
+        <li>[{event.datetime}] {event.type}</li>
+      {/each}
+    </ul>
+  {/await}
 </main>
 
 <style>
